2b72a11d85de45424394dbd17427626c645957ff
Nathan Willi de Souza Cortinaz <nathancortinaz@hotmail.com>
2022-04-10 22:35:14 -0300

    Code refactored, face analyzer class created and expressions being saved to Excel file

--- src/app.py
+++ src/app.py
@@ -1,82 +1,71 @@
 import cv2
-import eel
 import pandas as pd
-from deepface import DeepFace
 from time import sleep, perf_counter
+from deepface import DeepFace
 
 
-def checkCam():
-    '''Verifica conexão com webcam'''
-    capture = cv2.VideoCapture(0)
-    if not capture.isOpened():
-        raise IOError('Não foi possível acessar webcam')
-    return capture
+class FaceAnalyzer():
+    def __init__(self, capture, exit_character='q'):
+        self.capture = capture
+        if not self.capture.isOpened():
+            raise IOError("Não foi possível acessar webcam")
+        else:
+            self.start = perf_counter()
+            self.current_reaction = 'neutral'
+            self.new_reaction = 'neutral'
+            self.face_found = False
+            self.reactions = [(self.current_reaction, 0)]
+            self.faceCascade = cv2.CascadeClassifier(
+                cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+            self.exit_character = exit_character
+
+    def detect_reaction(self):
+        '''Check webcam video looking for face expressions'''
+        try:
+            success, self.frame = self.capture.read()
+            predictions = DeepFace.analyze(self.frame, actions=['emotion'])
+            self.new_reaction = predictions["dominant_emotion"]
+            print(self.new_reaction)
+            self.face_found = True
+        except:
+            print("Face not detected...")
+            self.face_found = False
+
+    def show_faces(self):
+        '''Show faces detected in webcam with their expressions'''
+        if self.face_found == True:
+            faces = self.faceCascade.detectMultiScale(self.frame, 1.1, 4)
+            for(x, y, w, h) in faces:
+                cv2.rectangle(self.frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                cv2.putText(self.frame, self.new_reaction, (x, y-10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_4)
+            cv2.imshow('FaceAnalyzer', self.frame)
+
+    def check_reaction(self):
+        '''Check if there is a new reaction and append it to reactions list'''
+        if self.new_reaction != self.current_reaction:
+            self.current_reaction = self.new_reaction
+            running_time = perf_counter() - self.start
+            self.reactions.append((self.new_reaction, running_time))
+
+    def check_stop(self):
+        '''Check if user wants to stop and save reactions to file'''
+        exitApp = cv2.waitKey(1) & 0xFF == ord(self.exit_character)
+        if exitApp:
+            print(f'{self.reactions = }')
+            capture.release()
+            cv2.destroyAllWindows()
+            results = pd.DataFrame(self.reactions)
+            results.to_excel("Results.xlsx", sheet_name='Detected Expresions')
+            exit()
 
 
-def faceDetector(capture):
-    '''Detecta faces nas imagens'''
-    # Função de busca por objetos sendo definida para buscar faces
-    faceCascade = cv2.CascadeClassifier(
-        cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    # Captura frame do streaming da webcam
-    success, frame = capture.read()
-    # Analisa a imagem em busca de faces
-    faces = faceCascade.detectMultiScale(frame, 1.1, 4)
-    # Identifica as expressões nas faces encontradas
-    predictions = DeepFace.analyze(frame, actions=['emotion'])
-    reaction = predictions["dominant_emotion"]
-    frame = outlineFaces(faces, frame, predictions)
-    cv2.imshow('Original', frame)
-    return reaction
-
-
-def checkReaction(new_reaction, reaction, reactions, start):
-    if new_reaction != reaction:
-        running_time = perf_counter() - start
-        reactions.append((new_reaction, running_time))
-    return reactions
-
-
-def outlineFaces(faces, frame, predictions):
-    '''Adiciona retângulos com expressão contornando as faces identificadas'''
-    for(x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.putText(frame, predictions["dominant_emotion"], (x, y-10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_4)
-    return frame
-
-
-def noFaceDetected():
-    '''Informa rosto não detectado'''
-    print("Tentando novamente...")
-
-
-def stopRunning(capture, reactions):
-    capture.release()
-    cv2.destroyAllWindows()
-    print(reactions)
-    results = pd.DataFrame(reactions)
-    writer = pd.ExcelWriter('Results.xlsx', engine='xlsxwriter')
-    results.to_excel(writer, sheet_name='welcome', index=False)
-    writer.save()
-
-
-# Início da aplicação
-exit_character = 'q'
-capture = checkCam()
-start = perf_counter()
-reaction = 'neutral'
-reactions = [(reaction, start)]
+capture = cv2.VideoCapture(0)
+fa = FaceAnalyzer(capture)
 
 while True:
-    # Try apenas para continuar tentando caso ocorra um erro de identificação de face
-    try:
-        new_reaction = faceDetector(capture)
-        reactions = checkReaction(new_reaction, reaction, reactions, start)
-    except ValueError:
-        noFaceDetected()
-    exitApp = cv2.waitKey(1) & 0xFF == ord(exit_character)
-    if exitApp:
-        stopRunning(capture, reactions)
-        break
-    sleep(1)
+    fa.detect_reaction()
+    fa.show_faces()
+    fa.check_reaction()
+    fa.check_stop()
+    sleep(2)
